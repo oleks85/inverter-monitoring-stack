12ed32cc423409c7113708bd1666945e1f6b6b98
Oleksandr.Kononenko@rci.rogers.com <Oleksandr.Kononenko@rci.rogers.com>
2022-12-26 17:13:38 +0200

    Moxa device in use over TCP

--- mppsolar/entrypoint.sh
+++ mppsolar/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # turn on bash's job control
-set -m
+#set -m
 
 # Start the primary process and put it in the background
 # MOXA_CONNECTION_STRING eq. tcp:192.168.1.22:4001
+echo MOXA_CONNECTION_STRING $MOXA_CONNECTION_STRING
 socat pty,link=/dev/moxa0,raw $MOXA_CONNECTION_STRING & 
   
 # Start the helper process
@@ -16,4 +17,4 @@ mpp-solar --configfile /etc/mpp-solar/mpp-solar.conf
   
 # now we bring the primary process back into the foreground
 # and leave it there
-# fg %2
+#fg %2
