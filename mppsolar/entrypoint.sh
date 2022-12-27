d3d6af1972d7b828e02f4d43a4519f90af7e6bb8
Oleksandr.Kononenko@rci.rogers.com <Oleksandr.Kononenko@rci.rogers.com>
2022-12-27 16:07:55 +0200

    Moxa device in use over TCP

--- mppsolar/entrypoint.sh
+++ mppsolar/entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # turn on bash's job control
-#set -m
+set -m
 
 # Start the primary process and put it in the background
 # MOXA_CONNECTION_STRING eq. tcp:192.168.1.22:4001
 #socat -d -d -d -d pty,link=/dev/moxa0,raw,echo=0 $MOXA_CONNECTION_STRING & 
-socat -d -d -d -s pty,link=/dev/ttyUSB0,raw,echo=0,ignoreeof TCP-LISTEN:4001,reuseaddr
+socat -d -d -d -s pty,link=/dev/ttyUSB0,raw,echo=0,ignoreeof TCP-LISTEN:4001,reuseaddr &
 # wait before open connection
-sleep 2
+sleep 1
 # Start the helper process
-mpp-solar --configfile /etc/mpp-solar/mpp-solar.conf &
+mpp-solar --configfile /etc/mpp-solar/mpp-solar.conf
 
 # the my_helper_process might need to know how to wait on the
 # primary process to start before it does its work and returns
 
-wait -n
-exit $?
+#wait -n
+#exit $?
 # now we bring the primary process back into the foreground
 # and leave it there
-#fg %2
+fg %1
